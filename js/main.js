93d81131b2dd5b24d5e80444d9f9d2fb0a0599f0
Seggy Umboh <seggy.umboh@gmail.com>
2012-12-18 18:41:09 -0800

    Push out arrows and auto hide them, and added keyboard control

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $(function() {
-  $('#slider ul').bxSlider({
-    auto: true
-  }).show();
+  $('section.content').hover(function() {
+    $('.bx-prev, .bx-next').show();
+  }, function() {
+    $('.bx-prev, .bx-next').hide();
+  });
+
+  var slider = $('#slider ul').bxSlider({
+    auto: true,
+    autoHover: true,
+    pause: 6000
+  });
+
+  $('body').keyup(function(e) {
+    if (37 == e.keyCode) {
+      slider.goToPrevSlide();
+    } else if (39 == e.keyCode) {
+      slider.goToNextSlide();
+    }
+  });
+
+  slider.show();
 });
